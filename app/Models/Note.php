09c7e486e501584337538b505d792e2982d1cb33
Rahamat Jahan <rahamatj92@gmail.com>
2025-09-12 01:29:39 +0600

    first commit

---
+++ app/Models/Note.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $guarded = ['id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function publishedNotes(User $user)
+    {
+        return $this->where('user_id', $user->id)
+                    ->where('is_published', true)
+                    ->get();
+    }
+}
